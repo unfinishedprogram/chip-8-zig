c460c763d33f09adf357f754f3ceae2fe034dba0
Noah <noah.labrecue@gmail.com>
2022-07-16 18:38:05 -0400

    removed example test

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,6 @@ const rom_reader = @import("rom_reader.zig");
 
 pub fn main() anyerror!void {
     const buffer = try rom_reader.getByteBufferFromFile("roms/pong.rom");
-    
     const buffer_small = rom_reader.getOpCodes(buffer);
-
     rom_reader.printOpCodes(buffer_small);
-    // std.debug.print("{s}\n", .{buffer_small});
-}
-
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
 }
